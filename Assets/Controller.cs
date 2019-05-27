54c8def9c196004ab94a39d1c2582052a4f82803
Sam Richmond <srichmond123@gmail.com>
2019-05-27 17:08:31 -0400

    Added non vr mode

---
+++ Assets/Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.XR;
+using Vector3 = UnityEngine.Vector3;
+
+
+public class Controller : MonoBehaviour {
+    public bool disableVR;
+
+    private float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start() {
+        if (disableVR) {
+            XRSettings.LoadDeviceByName("");
+            XRSettings.enabled = false;
+        }
+        else {
+            //
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (OVRInput.Get(OVRInput.Button.One)) { //A button pressed, right controller:
+            fly(speed * Time.deltaTime);
+        } else if (OVRInput.Get(OVRInput.Button.Two)) {
+            fly(-speed * Time.deltaTime);
+        }
+    }
+
+    // "fly" meaning translate position in direction camera is facing
+    void fly(float speed) {
+        transform.localPosition += transform.GetChild(0).GetChild(0).forward * speed;
+    }
+}
